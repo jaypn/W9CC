09967d1ecb597582469cc10988552f38d532c7a3
nyabuganda <jayp@nyabugandas-MacBook-Pro.local>
2020-08-20 22:39:42 -0600

    created array of twwets

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var tweet = ["exams be like:if a ladder of 400cm stands at an abtuse angle and a man that weights 80 kgs climds. what is his birthday!","Dear Math please grow up and resolve your own problems.", "Multiple smallPRs so your colleages can review your code","Cutting tennis balls in half lets you store more balls saving space.","i think i can make a website company, me afeter completing html an scss", "raise up your hands if you think you are not crazy", "call me by your name", "i will be calling you everyday to know if you are doing good!","if you didnt spend 6hours automating a task tahat takes takes you 30 secondes did you really do work?", "I dont like my life anymore"];
+
+
+for(var index=0; index < tweet.length; index++) {
+    console.log(tweet[index]);
+}
